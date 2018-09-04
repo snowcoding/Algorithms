f4282fc4a98fb1e39113955fbe6f986d8e6a1fc3
snowcoding <vimalshah77@gmail.com>
2018-09-04 11:36:04 -0700

    Basic climbing stairs with -small dataset O(3^n)

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -3,14 +3,22 @@
 import sys
 
 def climbing_stairs(n):
-  pass
+    if n == 0 or n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    elif n == 3:
+        return 4
+    else:
+        return(climbing_stairs(n-1) + climbing_stairs(n-2) + climbing_stairs(n-3))
 
+print(climbing_stairs(5))
 
 if __name__ == "__main__":
-  # Test out your implementation from the command line
-  # with `python climbing_stairs.py [n]` with different n values
-  if len(sys.argv) > 1:
-    num_stairs = int(sys.argv[1])
-    print("There are {ways} ways for a child to jump {n} stairs.".format(ways=climbing_stairs(num_stairs), n=num_stairs))
-  else:
-    print('Usage: climbing_stairs.py [num_stairs]')
+    # Test out your implementation from the command line
+    # with `python climbing_stairs.py [n]` with different n values
+    if len(sys.argv) > 1:
+        num_stairs = int(sys.argv[1])
+        print("There are {ways} ways for a child to jump {n} stairs.".format(ways=climbing_stairs(num_stairs), n=num_stairs))
+    else:
+        print('Usage: climbing_stairs.py [num_stairs]')
